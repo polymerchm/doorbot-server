e9b17c080eb08c546392059ed815faf525d42348
Timm Murray <tmurray@wumpus-cave.net>
2021-11-20 14:58:00 -0600

    Fetch all members with pagination

--- build_active_member_cache.py
+++ build_active_member_cache.py
@@ -26,5 +26,29 @@ def fetch_member_page(
         return data
 
 
-members = fetch_member_page()
-print( f'Fetched {len( members )} members' )
+def fetch_all_members():
+    is_still_more = True
+    page = 1
+    per_page = 100
+    all_members = []
+
+    while is_still_more:
+        members = fetch_member_page( page, per_page )
+        print( f'Fetched {len( members )} members' )
+
+        if len( members ) != per_page:
+            # If we didn't get as many members as expected, assume we've 
+            # reached the end of the list. Unfortunately, the MemberPress
+            # API doesn't have any other way for us to know this.
+            is_still_more = False
+            print( f'Reached the end of the member list on page {page}' )
+
+        if len( members ) != 0:
+            all_members = all_members +  members
+
+        page += 1
+
+    print( f'Fetched {len( all_members )} members' )
+
+
+fetch_all_members()
