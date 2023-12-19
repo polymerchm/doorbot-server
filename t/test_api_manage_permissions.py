b722fa3b136724002dbb07b095112d5e48181d33
tmurray <tmurray@wumpus-cave.net>
2023-12-18 19:01:35 -0600

    Tweak API to use DELETE more intellegently. Lay out tests for managing permissions.

--- t/test_api_manage_permissions.py
+++ t/test_api_manage_permissions.py
@@ -13,7 +13,7 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 
-USER_PASS = ( "user", "pass" )
+USER1 = "1234"
 
 class TestAPIManagePermissions( flask_unittest.ClientTestCase ):
     app = Doorbot.API.app
@@ -29,9 +29,57 @@ class TestAPIManagePermissions( flask_unittest.ClientTestCase ):
 
         member = Doorbot.SQLAlchemy.Member(
             full_name = "_tester",
-            rfid = USER_PASS[0],
+            rfid = USER1,
         )
 
         session = Session( engine )
         session.add( member )
         session.commit()
+
+    def test_manage_permission( self, client ):
+        # TODO
+        return
+
+        # User starts with no permission
+        rv = client.get( '/secure/check_tag/' + USER1 + '/foo.permission' )
+        self.assertStatus( rv, 403 )
+
+        # Creates the role and the permission at once
+        rv = client.put( '/secure/permission/foo.permission/foo_role' )
+        self.assertStatus( rv, 201 )
+
+        # User still has no permission
+        rv = client.get( '/secure/check_tag/foo.permission/' + USER1 )
+        self.assertStatus( rv, 403 )
+
+        # Add role to user
+        rv = client.put( '/secure/role/foo_role/' + USER1 )
+        self.assertStatus( rv, 201 )
+
+        # User still has permission
+        rv = client.get( '/secure/check_tag/' + USER1 + '/foo.permission' )
+        self.assertStatus( rv, 200 )
+
+        # Remove permission from role
+        rv = client.delete( '/secure/permission/foo.permission/foo_role' )
+        self.assertStatus( rv, 200 )
+
+        # User has no permission
+        rv = client.get( '/secure/check_tag/' + USER1 + '/foo.permission' )
+        self.assertStatus( rv, 403 )
+
+        # Set permission again on role
+        rv = client.put( '/secure/permission/foo.permission/foo_role' )
+        self.assertStatus( rv, 201 )
+
+        # User has permission again
+        rv = client.get( '/secure/check_tag/' + USER1 + '/foo.permission' )
+        self.assertStatus( rv, 200 )
+
+        # Remove role from user
+        rv = client.delete( '/secure/role/foo_role/' + USER1 )
+        self.assertStatus( rv, 200 )
+
+        # User no longer has permission
+        rv = client.get( '/secure/check_tag/' + USER1 + '/foo.permission' )
+        self.assertStatus( rv, 403 )
