cadefca35345a9ceda41cbd1d93219e39a12c8f7
Timm Murray <tmurray@wumpus-cave.net>
2024-07-21 14:34:19 +0000

    Add a hostname to the locations so we can link them on the UI

--- sql/changes_pg.sql
+++ sql/changes_pg.sql
@@ -1,8 +1,1 @@
-CREATE TABLE oauth_tokens (
-    id SERIAL PRIMARY KEY NOT NULL,
-    name TEXT NOT NULL,
-    token TEXT NOT NULL UNIQUE,
-    expiration_date TIMESTAMP WITH TIME ZONE NOT NULL,
-    member_id INT NOT NULL REFERENCES members (id)
-);
-CREATE INDEX ON oauth_tokens (member_id);
+ALTER TABLE locations ADD hostname TEXT;
